5e2ec1821ee42b0f3d46a5d9e01d1ac117491c28
Stanley Louis <stanlouis@msn.com>
2018-06-10 07:03:21 -0400

    Adding middleware and maintenance page

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 const app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
-app.use(express.static(__dirname + "./public"));
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile(
+    "server.log",
+    `${log}
+`,
+    err => {
+      if (err) {
+        console.log(err, "Unable to append to server.log");
+      }
+    }
+  );
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
+
+app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
 hbs.registerHelper("screamIt", text => text.toUpperCase());
